0825a6673cddf60a2db1812daa10b2a25db185e3
Philippe Dumonet <philippe@dumo.net>
2023-10-04 17:42:16 +0200

    ♻️ Simplify files

--- src/see-counter.ts
+++ src/see-counter.ts
@@ -2,19 +2,35 @@ import {
   createAztecRpcClient,
   getSchnorrAccount,
   GrumpkinScalar,
-  Wallet
+  Wallet,
+  AztecRPC,
+  AztecAddress,
+  Fr
 } from '@aztec/aztec.js'
 import { envGet, hexToBuffer, toAddr } from './utils.js'
 import { PrivateCounterContract } from './contracts/types/PrivateCounter.js'
 
 const SANDBOX_URL = process.env['SANDBOX_URL'] || 'http://localhost:8080'
-const PRIV_KEY = hexToBuffer(envGet('PRIV_KEY'))
+const PRIV_KEY = hexToBuffer(envGet('PRIV_KEY_1'))
 
 const MAIN_ADDR = toAddr(envGet('MAIN_ADDR'))
 const COUNTER_ADDR = toAddr(envGet('COUNTER_ADDR'))
 
 async function getCounter(client: Wallet) {
   return await PrivateCounterContract.at(COUNTER_ADDR, client)
+}
+
+async function deployCounter(
+  rpc: AztecRPC,
+  addr: AztecAddress,
+  salt: Fr
+): Promise<PrivateCounterContract> {
+  const res = await PrivateCounterContract.deploy(rpc, 100, addr)
+    .send({
+      contractAddressSalt: salt
+    })
+    .wait()
+  return res.contract
 }
 
 async function main() {
@@ -34,6 +50,7 @@ async function main() {
   ).getWallet()
   console.log('account.address:', account.getAddress().toString())
 
+  // const counter = await deployCounter(rpc, account.getAddress(), Fr.random())
   const counter = await getCounter(account)
 
   const getCount = async (c: PrivateCounterContract, f: string) => {
@@ -47,14 +64,23 @@ async function main() {
 
   await getCount(counter, 'flag')
 
-  const tx = await counter.methods
-    .increment(account.getAddress().toField())
+  console.log('pre tx')
+  console.log('account.getAddress():', account.getAddress())
+  console.log(
+    'account.getAddress().toString():',
+    account.getAddress().toString()
+  )
+  console.log('account.getAddress().toField():', account.getAddress().toField())
+  await counter
+    .withWallet(account)
+    .methods.increment(account.getAddress().toField())
     .send()
-  console.log(`Sent transfer transaction ${await tx.getTxHash()}`)
+    .wait()
+  // console.log(`Sent transfer transaction ${await tx.getTxHash()}`)
 
-  console.log('Awaiting transaction to be mined')
-  const receipt = await tx.wait()
-  console.log(`Transaction has been mined on block ${receipt.blockNumber}`)
+  // console.log('Awaiting transaction to be mined')
+  // const receipt = await tx.wait()
+  // console.log(`Transaction has been mined on block ${receipt.blockNumber}`)
 
   await getCount(counter, 'lol')
 }
