b625ce2a437b91e4693b9aab2787e069f161a139
Philippe Dumonet <philippe@dumo.net>
2023-10-04 16:54:44 +0200

    🔨 Add all private keys

--- src/test/PrivateCounter.test.ts
+++ src/test/PrivateCounter.test.ts
@@ -4,77 +4,78 @@ import {
   AztecRPC,
   CompleteAddress,
   getSchnorrAccount,
-  GrumpkinScalar
+  GrumpkinScalar,
+  AccountWallet
 } from '@aztec/aztec.js'
 import { PrivateCounterContract } from '../contracts/types/PrivateCounter.js'
 import { envGet, hexToBuffer } from '../utils.js'
 
 const { SANDBOX_URL = 'http://localhost:8080' } = process.env
-const PRIV_KEY = hexToBuffer(envGet('PRIV_KEY'))
+const PRIV_KEYS = [1, 2, 3].map((i) => hexToBuffer(envGet(`PRIV_KEY_${i}`)))
 
 describe('PrivateCounter', () => {
   const initialOwnerCount = 100n
   let rpc: AztecRPC
   let salt: Fr
-  let accounts: CompleteAddress[]
+  let wallets: AccountWallet[]
   let counter: PrivateCounterContract
 
   beforeAll(async () => {
     rpc = createAztecRpcClient(SANDBOX_URL)
 
-    accounts = await rpc.getRegisteredAccounts()
+    const completeAddrs = await rpc.getRegisteredAccounts()
 
-    const deployerWallet = accounts[0]
+    wallets = await Promise.all(
+      completeAddrs.map(async (completeAddr: CompleteAddress, i: number) => {
+        const skGrumpScalar = GrumpkinScalar.fromBuffer(PRIV_KEYS[i])
+        return await getSchnorrAccount(
+          rpc,
+          skGrumpScalar,
+          skGrumpScalar,
+          completeAddr
+        ).getWallet()
+      })
+    )
+
+    const deployerWallet = wallets[0]
     salt = Fr.random()
 
     counter = await PrivateCounterContract.deploy(
       rpc,
       initialOwnerCount,
-      deployerWallet.address
+      deployerWallet.getAddress().toField()
     )
       .send({ contractAddressSalt: salt })
       .deployed()
   })
 
   it('users starts with correct counter', async () => {
-    const [owner, other] = accounts
+    const [owner, other] = wallets
 
     expect(
-      await counter.methods.get_counter(owner.address.toField()).view()
+      await counter.methods.get_counter(owner.getAddress().toField()).view()
     ).toEqual(initialOwnerCount)
 
     expect(
-      await counter.methods.get_counter(other.address.toField()).view()
+      await counter.methods.get_counter(other.getAddress().toField()).view()
     ).toEqual(0n)
   })
 
   it('can increase counter', async () => {
-    const [owner] = accounts
+    const [owner] = wallets
 
     expect(
-      await counter.methods.get_counter(owner.address.toField()).view()
+      await counter.methods.get_counter(owner.getAddress().toField()).view()
     ).toEqual(initialOwnerCount)
 
-    const mainGrumpScalar = GrumpkinScalar.fromBuffer(PRIV_KEY)
-    const ownerWallet = await getSchnorrAccount(
-      rpc,
-      mainGrumpScalar,
-      mainGrumpScalar,
-      owner
-    ).getWallet()
-
-    expect(owner.address.toString()).toEqual(
-      ownerWallet.getAddress().toString()
-    )
-
     await counter
-      .withWallet(ownerWallet)
-      .methods.increment(owner.address.toField())
+      .withWallet(owner)
+      .methods.increment(owner.getAddress().toField())
       .send()
       .wait()
 
     expect(
-      await counter.methods.get_counter(owner.address.toField()).view()
+      await counter.methods.get_counter(owner.getAddress().toField()).view()
     ).toEqual(initialOwnerCount + 1n)
   })
 })
